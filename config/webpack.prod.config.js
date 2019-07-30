49a3e8eea9c68072e461cd65260573947281e1c2
Jaryl Sim <jaryl@tinkerbox.com.sg>
2019-07-30 23:12:01 +0800

    Use babel transforms to save bundle size.

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 
 const merge = require('webpack-merge');
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const common = require('./webpack.common.config.js');
 
 const config = {
