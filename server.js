371e7cd2e50eed83fb6f5b01f290d3875a8ee37d
Jaryl Sim <jaryl@tinkerbox.com.sg>
2019-07-31 16:31:56 +0800

    Use helmet defaults for protecting express server.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(express.static('dist'));
 
 app.listen(port, () => console.log(`Mailyard SPA listening on port ${port}!`));
