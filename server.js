c5ff2abf929adad2e59af6a9a0bc61ad208656bf
Jaryl Sim <jaryl@tinkerbox.com.sg>
2019-08-02 16:58:19 +0800

    Switching over to Next.js.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const next = require('next');
 const helmet = require('helmet');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(helmet());
-app.use(express.static('dist'));
+const nextApp = next({
+  dev: (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'staging'),
+});
+const handler = nextApp.getRequestHandler();
 
-app.listen(port, () => console.log(`Mailyard SPA listening on port ${port}!`));
+nextApp.prepare()
+  .then(() => {
+    app.use(helmet());
+    // app.use(express.static('dist'));
+    app.get('*', handler);
+    app.listen(port, () => console.log(`Mailyard SPA listening on port ${port}!`));
+  })
+  .catch((error) => {
+    console.error(error.stack);
+    process.exit(1);
+  });
+
