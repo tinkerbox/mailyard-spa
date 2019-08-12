780fa0345676b2a84f0dd0913072d289f5cb3a47
Jaryl Sim <jaryl@tinkerbox.com.sg>
2019-08-13 00:58:21 +0800

    Disable pretty output to save bandwidth on Google API requests.

--- utils/google-api.js
+++ utils/google-api.js
@@ -1,26 +1,42 @@
 /* eslint-disable arrow-body-style */
 
+import { map, join } from 'lodash';
+
 const GOOGLE_API_BASE = 'https://www.googleapis.com/gmail/v1';
 
 function GoogleApi(client) {
+  // private interface
+
+  const defaultParams = {
+    prettyPrint: false,
+  };
+
+  const formatParams = (params = {}) => {
+    const combined = { ...defaultParams, ...params };
+    const parts = map(combined, (val, id) => { return `${id}=${val}` });
+    return join(parts, '&');
+  };
+
+  // public interface
+
   this.client = client;
 
   this.getAllLabels = () => {
-    return client.request({ path: `${GOOGLE_API_BASE}/users/me/labels` });
+    return client.request({ path: `${GOOGLE_API_BASE}/users/me/labels?${formatParams()}` });
   };
 
   this.getProfile = () => {
-    return client.request({ path: `${GOOGLE_API_BASE}/users/me/profile` });
+    return client.request({ path: `${GOOGLE_API_BASE}/users/me/profile?${formatParams()}` });
   };
 
-  this.getAllMessages = (token) => {
-    const params = token ? `pageToken=${token}` : '';
-    return client.request({ path: `${GOOGLE_API_BASE}/users/me/messages?${params}` });
+  this.getAllMessages = (pageToken) => {
+    const params = pageToken ? { pageToken } : {};
+    return client.request({ path: `${GOOGLE_API_BASE}/users/me/messages?${formatParams(params)}` });
   };
 
   this.getMessage = (id) => {
-    const params = 'format=raw';
-    return client.request({ path: `${GOOGLE_API_BASE}/users/me/messages/${id}?${params}` });
+    const params = { format: 'raw' };
+    return client.request({ path: `${GOOGLE_API_BASE}/users/me/messages/${id}?${formatParams(params)}` });
   };
 }
 
