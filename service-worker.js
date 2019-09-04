05d2ffa638dff19798945b4364ca9e345be26f76
Jaryl Sim <jaryl@tinkerbox.com.sg>
2019-09-04 15:19:13 +0800

    Assets from S3 are now cached for 30 days.

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,25 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+const APP_NAME = 'mailyard';
+const WORKER_VER = 'v1';
+
+workbox.routing.registerRoute(
+  /.*amazonaws.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: `${APP_NAME}-${WORKER_VER}-s3`,
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 320,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // cache for 30 days
+        purgeOnQuotaError: true,
+      }),
+      new workbox.cacheableResponse.Plugin({
+        statuses: [200],
+      }),
+    ],
+    matchOptions: {
+      ignoreSearch: true,
+    },
+  }),
+);
