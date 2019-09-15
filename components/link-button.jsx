e7442eb0a683634601f8301dfd66bd345a2f8ddc
Jaryl Sim <jaryl@tinkerbox.com.sg>
2019-09-16 00:41:22 +0800

    Proper use of NextJS link in LinkButton, takes advantage of pre-fetch.

--- components/link-button.jsx
+++ components/link-button.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
+import Link from 'next/link';
 
-const LinkButton = React.forwardRef(({ onClick, href, children, ...others }, ref) => {
-  return <Button onClick={onClick} href={href} ref={ref} {...others}>{children}</Button>;
-});
+const LinkButton = React.memo(({ href, children, ...others }) => (
+  <Link href={href}>
+    <Button {...others}>{children}</Button>
+  </Link>
+));
 
 LinkButton.propTypes = {
-  onClick: PropTypes.func,
   href: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
-};
-
-LinkButton.defaultProps = {
-  onClick: () => { },
 };
 
 LinkButton.whyDidYouRender = true;
