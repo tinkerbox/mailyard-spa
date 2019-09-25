496231995ce263ba505e7b49ba240f2c8db46047
Jaryl Sim <jaryl@tinkerbox.com.sg>
2019-09-25 16:01:12 +0800

    Cleanup for unecessary presentation code.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -18,8 +18,6 @@ class SiteDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           {this.props.styleTags}
         </Head>
         <body className="root">
