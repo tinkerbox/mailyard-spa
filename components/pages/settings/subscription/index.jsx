18bd75b3d328e59c83c0403fb66a7a24cefcd6b5
Jaryl Sim <jaryl@tinkerbox.com.sg>
2019-10-02 14:57:05 +0800

    Add link to login in to billing portal.

--- components/pages/settings/subscription/index.jsx
+++ components/pages/settings/subscription/index.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, Spin } from 'antd';
+import gql from 'graphql-tag';
+import { ApolloContext } from 'react-apollo';
+
+import config from '../../../../config/runtime';
+
+const BILLING_TOKEN_MUTATATION = gql`
+  mutation {
+  billingToken{
+    token
+  }
+}
+`;
 
 const Subscription = () => {
-  return <p>Subscription</p>;
+  const { client } = useContext(ApolloContext);
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    client.mutate({
+      mutation: BILLING_TOKEN_MUTATATION,
+    }).then(({ data }) => {
+      setToken(data.billingToken.token);
+    }).catch((err) => {
+      console.error(err);
+    });
+  }, [client]);
+
+  return (
+    <React.Fragment>
+      <br />
+      <p>Show subscription status here.</p>
+      {token && <Button href={`${config.MAILYARD_WEB_URL}/billing/api/auth?token=${token}`}>Billing Portal</Button>}
+      {!token && <Spin />}
+    </React.Fragment>
+  );
 };
 
 Subscription.whyDidYouRender = true;
